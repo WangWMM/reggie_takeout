b01e56586f26a1e37d86a1102dcbbe9a028f28c3
wang <wangmin13@sunland.com>
2022-12-29 16:07:01 +0800

    <fix>(MyBatisPlusConfig)

---
+++ src/main/java/com/itheima/reggie/config/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.itheima.reggie.config;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * @author Wangmin
+ * @date 2022/12/28 17:49
+ */
+
+
+/**
+ * 配置mp的分页插件
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
